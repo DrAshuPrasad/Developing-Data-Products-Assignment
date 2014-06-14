ce0962b64ee2067f5a60864275aa1c7d5ca4a449
Raymond Tang <raymondtang@192-168-1-7.tpgi.com.au>
2014-06-14 16:30:27 +1000

    added more calculations needed to make decision on buying a property

--- ui.R
+++ ui.R
@@ -2,13 +2,23 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Property Hunter"),
   sidebarPanel(
+    h4('Property'),
     textInput('text', 'Address', value=''),
-    numericInput('price', 'Property Price ($)', 0),
-    numericInput('weeklyRent', 'Weekly Rent ($)', 0)
+    numericInput('price', 'Price ($)', 550000),
+    numericInput('weeklyRent', 'Weekly Rent ($)', 550),
+    h5('Costs'),
+    numericInput('weeklyRepayments', 'Weekly Repayments ($ per week)', 503),
+    numericInput('strataPerQuarter', 'Strata ($ per quarter)', 1050),
+    numericInput('councilPerQuarter', 'Council ($ per quarter)', 163),
+    numericInput('waterPerQuarter', 'Water ($ per quarter)', 180),
+    numericInput('managementFees', 'Management Fees ($ per week)', 38)
   ),
   mainPanel(
-    h3('Rental Yield'),
-    code('some code'),
-    verbatimTextOutput("rentalYield")
+    h4('Rental Yield (%)'),
+    textOutput("rentalYield"),
+    h4('Cashflow Per Week ($)'),
+    textOutput("cashflowPerWeek"),
+    h4('Cashflow Per Year ($)'),
+    textOutput("cashflowPerYear")
   )
 ))
